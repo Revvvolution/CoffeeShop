c45df763283dc4256fcd359733de8b73e9ec25d1
Rev Huffman <148085981+Revvvolution@users.noreply.github.com>
2024-09-05 14:24:01 -0400

    Completed GetAll method for CoffeeRepository

--- CoffeeShop/Models/Coffee.cs
+++ CoffeeShop/Models/Coffee.cs
@@ -1,19 +1,19 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using CoffeeShop.Models;
 
 namespace CoffeeShop.Models
 {
-    public class BeanVariety
+    public class Coffee
     {
         public int Id { get; set; }
 
         [Required]
         [StringLength(50, MinimumLength = 3)]
-        public string Name { get; set; }
+        public string Title { get; set; }
 
         [Required]
-        [StringLength(255, MinimumLength = 3)]
-        public string Region { get; set; }
+        public int BeanVarietyId { get; set; }
 
-        public string? Notes { get; set; }
+        public BeanVariety BeanVariety { get; set; }
     }
 }
---
+++ CoffeeShop/Repositories/CoffeeRepository.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using CoffeeShop.Models;
+
+namespace CoffeeShop.Repositories
+{
+    public class CoffeeRepository : ICoffeeRepository
+    {
+        private readonly string _connectionString;
+        public CoffeeRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+
+        private SqlConnection Connection
+        {
+            get { return new SqlConnection(_connectionString); }
+        }
+
+        public List<Coffee> GetAll()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT c.Id, c.Title, c.BeanVarietyId, bv.Id AS 'BV Id', bv.[Name] AS 'Bean Variety Name', bv.Region, bv.[Notes]
+                                        FROM Coffee c
+                                        LEFT OUTER JOIN BeanVariety bv
+                                        ON c.BeanVarietyId = bv.Id
+                    ;";
+
+                    var reader = cmd.ExecuteReader();
+                    var coffeeList = new List<Coffee>();
+                    while (reader.Read())
+                    {
+                        var coffee = new Coffee()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Title = reader.GetString(reader.GetOrdinal("Title")),
+                            BeanVarietyId = reader.GetInt32(reader.GetOrdinal("BeanVarietyId")),
+                            BeanVariety = new BeanVariety
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("BV Id")),
+                                Name = reader.GetString(reader.GetOrdinal("Bean Variety Name")),
+                                Region = reader.GetString(reader.GetOrdinal("Region")),
+                                Notes = reader.IsDBNull(reader.GetOrdinal("Notes")) ? null :
+                                        reader.GetString(reader.GetOrdinal("Notes")),
+                            }
+                        };
+                        coffeeList.Add(coffee);
+                    }
+
+                    reader.Close();
+
+                    return coffeeList;
+                }
+            }
+        }
+
+        public Coffee Get(int id)
+        {
+            return null;
+        }
+
+        public void Add(Coffee coffee)
+        {
+            return;
+        }
+
+        public void Update(Coffee coffee)
+        {
+            return;
+        }
+
+        public void Delete(int id)
+        {
+            return;
+        }
+
+
+    }
+}
