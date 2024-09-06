2cdea1b26e879296c2284db1487b012a07ab8e04
Rev Huffman <148085981+Revvvolution@users.noreply.github.com>
2024-09-06 13:57:07 -0400

    Updated main.js to include fundction for printing list of bean varieties to the program

--- js-app/main.js
+++ js-app/main.js
@@ -1,13 +1,34 @@
-const url = "https://localhost:5001/api/beanvariety/";
+const beanVarietyUrl = "https://localhost:5001/api/beanvariety/";
 
 const button = document.querySelector("#run-button");
+const container = document.querySelector("#container");
+
+    const printBeanVarieties = (beanVariety) => {
+        const composedHTML_Content = `
+            <section class="bv_object">Bean Varieties
+                <h3>${beanVariety.Name}</h3>
+                <ul>
+                    <li>Region: ${beanVariety.Region}</li>
+                    <li>Notes: ${beanVariety.Notes}</li>
+                </ul>
+            </section>
+            <br>
+        `;
+
+        container.innerHTML += composedHTML_Content
+    };
+
+
 button.addEventListener("click", () => {
+    const composedHTML_Header = `<h1>Bean Varieties</h1>`;
+    container.innerHTML = composedHTML_Header
     getAllBeanVarieties()
         .then(beanVarieties => {
             console.log(beanVarieties);
+            printBeanVarieties(beanVarieties);
         })
 });
 
 function getAllBeanVarieties() {
-    return fetch(url).then(resp => resp.json());
+    return fetch(beanVarietyUrl).then(resp => resp.json());
 }
